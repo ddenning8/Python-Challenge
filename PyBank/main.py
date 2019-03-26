9467e67dadb3d5438479023468b787cabeaa3fd6
ddenning8 <ddenning87@gmail.com>
2019-03-26 16:37:46 -0400

    new save

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,8 +4,16 @@ import csv
 
 budget_data_csv = os.path.join("../PyBank","budget_data.csv")
 
+print("Financial Analysis")
+
+print("----------------------------------")
+
 with open(budget_data_csv, newline = "") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
-    for row in csvreader:
-        print(row)
-        
+    
+    Total_Months = sum(1 for line in csvfile) - 1
+    print("Total_Months: " + str(Total_Months))
+
+
+
+ 
