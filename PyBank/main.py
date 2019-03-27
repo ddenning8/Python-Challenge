963d4b382a7fd5878368283a070d7427c03bf7f9
ddenning8 <ddenning87@gmail.com>
2019-03-27 11:34:24 -0400

    Next Save

--- PyBank/main.py
+++ PyBank/main.py
@@ -35,3 +35,17 @@ for i in range(len(Total) - 1): #-1 b/c last row point can't be subtracted
     Monthly_Change.append(Total[i+1] - Total[i])
 
 print(f"Average Change: ${round(sum(Monthly_Change)/len(Monthly_Change) , 2)}")
+
+#Find max and min totals from the previous list.
+Max_Total_Value = max(Monthly_Change)
+Min_Total_Value = min(Monthly_Change)
+
+#Reference that max and min number to the beginning month AKA index. need +1 because change associated with next month
+Max_Month = Monthly_Change.index(max(Monthly_Change)) + 1
+Min_Month = Monthly_Change.index(min(Monthly_Change)) + 1 
+
+print(f"Greatest Increase in Profits: {Total_Months[Max_Month]} (${(str(Max_Total_Value))})")
+print(f"Greatest Decrease in Profits: {Total_Months[Min_Month]} (${(str(Min_Total_Value))})")
+
+
+
