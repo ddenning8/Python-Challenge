0e57eeff4b2c5d73ddb8eee2dfdf7ba3a2c66f80
ddenning8 <ddenning87@gmail.com>
2019-03-27 00:22:39 -0400

    next save

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,37 @@
-#open csv
+#import dependencies
 import os
 import csv
 
-budget_data_csv = os.path.join("../PyBank","budget_data.csv")
+#file location
+budget_data_csv = os.path.join("../PyBank", "budget_data.csv")
 
+#what are the things I need?
+Total_Months = []
+Total = []
+
+#open file to read
+with open(budget_data_csv, newline = "") as budget_data:
+    csvreader = csv.reader(budget_data, delimiter = ",")
+
+    #skip column names
+    header = next(csvreader)
+
+    #get data into correct lists
+    for rows in csvreader:
+        Total_Months.append(rows[0])
+        Total.append(int(rows[1]))
+
+#print what I got so far
 print("Financial Analysis")
+print("---------------------------------")
+print(f"Total Months: {len(Total_Months)}")
+print(f"Total: ${sum(Total)}")
 
-print("----------------------------------")
+#get change between the two months
+Monthly_Change = []
 
-with open(budget_data_csv, newline = "") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ",")
-    
-    Total_Months = sum(1 for line in csvfile) - 1
-    print("Total_Months: " + str(Total_Months))
+#go through rows and pull difference
+for i in range(len(Total) - 1): #-1 b/c last row point can't be subtracted
+    Monthly_Change.append(Total[i+1] - Total[i])
 
-
-
- 
+print(f"Average Change: ${round(sum(Monthly_Change)/len(Monthly_Change) , 2)}")
