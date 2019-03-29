bf6bfbe113212c633ddcf2eaaef270e51087e5b6
ddenning8 <ddenning87@gmail.com>
2019-03-28 21:24:12 -0400

    save

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,7 +5,7 @@ import csv
 #file location
 election_data_csv = os.path.join("../PyPoll", "election_data.csv")
 
-#Need total votes and candidates
+#what are the things I need to get?
 Total_Votes = 0
 Candidates = []
 Candidate_Votes = []
@@ -24,7 +24,7 @@ with open(election_data_csv, newline = "") as election_data:
     #pull out total votes
     for row in csvreader:
         Total_Votes += 1 #summing rows as total votes
-        if row[2] not in Candidates: #If not in empty list add cand and count a 1 for vote
+        if row[2] not in Candidates: #If not in empty list that was created, add candidate and count a 1 for vote
             Candidates.append(row[2])
             index = Candidates.index(row[2])
             Candidate_Votes.append(1)
@@ -38,10 +38,12 @@ with open(election_data_csv, newline = "") as election_data:
         Percent_New = "%.3f%%" % Percent #convert percent to 3 decimal places... found that on the interwebs
         Percent_Votes.append(Percent_New)
 
+    #Figure out who is the winner
     Winning_Candidate = max(Candidate_Votes)
     index = Candidate_Votes.index(Winning_Candidate)
     Winner = Candidates[index]
 
+#Print out the results for each candidate
 for i in range(len(Candidates)):
     print(f"{Candidates[i]}: {str(Percent_Votes[i])} ({str(Candidate_Votes[i])})")
 print("-----------------------")
@@ -49,6 +51,18 @@ print("-----------------------")
 print(f"Winner: {Winner}")
 
 print("-----------------------")
+
+
+with open("output.txt", "w") as output:
+    print("Election Results", file=output)
+    print("-----------------------", file=output)
+    print(f"Total Votes: {str(Total_Votes)}", file=output)
+    for i in range(len(Candidates)):
+        print(f"{Candidates[i]}: {str(Percent_Votes[i])} ({str(Candidate_Votes[i])})", file=output)
+    print("-----------------------", file=output)
+    print(f"Winner: {Winner}", file=output)
+    print("-----------------------", file=output)
+
     
 
 
